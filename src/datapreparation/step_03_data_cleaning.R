be827d52b8f4feb4f296e1ee4a08783352b68fc1
ldaniel <contato@leandrodaniel.com>
2019-08-24 16:36:18 -0300

    Update initial code before starting the development

--- src/datapreparation/step_03_data_cleaning.R
+++ src/datapreparation/step_03_data_cleaning.R
@@ -7,4 +7,4 @@ mytable_na_cols <- sapply(mytable, function(x) sum(is.na(x)))
 mytable_empty_cols <-  sapply(mytable, function(x) table(as.character(x) == "" | as.character(x) == " ")["TRUE"])
 
 # fixing NAs and missing values
-mytable <- mutate(mytable, x_importance = ifelse(is.na(x_importance), "1", x_importance))
+#mytable <- mutate(mytable, x_importance = ifelse(is.na(x_importance), "1", x_importance))
--- src/datapreparation/step_06_dataset_preparation.R
+++ src/datapreparation/step_06_dataset_preparation.R
@@ -9,9 +9,9 @@ source_dataset <- mytable
 # calling function to split and create train and test databases
 # this function will split the dataset into train and test data and save the sampling in disk
 # to resample just delete './models/source_train_test_dataset.rds' file and rerun this script
-if (file.exists('./models/source_train_test_dataset.rds')) {
-  source_train_test_dataset <- readRDS('./models/source_train_test_dataset.rds')
-} else {
-  source_train_test_dataset <- fgvr::createTestAndTrainSamples(source_dataset, "x_importance", 12345, 0.7)
-  saveRDS(source_train_test_dataset, './models/source_train_test_dataset.rds')  
-}
+# if (file.exists('./models/source_train_test_dataset.rds')) {
+#   source_train_test_dataset <- readRDS('./models/source_train_test_dataset.rds')
+# } else {
+#   source_train_test_dataset <- fgvr::createTestAndTrainSamples(source_dataset, "x_importance", 12345, 0.7)
+#   saveRDS(source_train_test_dataset, './models/source_train_test_dataset.rds')  
+# }
