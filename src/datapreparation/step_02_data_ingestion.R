ceeaffef5875d9857b928fd204395ffd5efb3386
ldaniel <contato@leandrodaniel.com>
2019-08-24 17:36:06 -0300

    Update with data ingestion (shapefile)

--- src/datapreparation/step_02_data_ingestion.R
+++ src/datapreparation/step_02_data_ingestion.R
@@ -1,11 +1,5 @@
 # performing data loading -----------------------------------------------------
-dataDirectory <- "data/raw/"
+dataDirectory <- "./data/raw/"
+shapefile_to_read <- paste(dataDirectory, "crime_mg.shp", sep = "")
 
-# The sample dataset is intended for demostration purpose only
-# Please, remove this whole comment section and the sample dataset
-# before starting your final project version.
-#
-# About the sample data set:
-# https://github.com/washingtonpost/data-game-of-thrones-deaths
-
-mytable <- "to-do"
+target <- shapefile(shapefile_to_read)
