a9895c676223e8af7ac7f52dacb9d15457b21c07
ldaniel <contato@leandrodaniel.com>
2019-08-25 18:18:02 -0300

    Adjusting rows with zero in POP_RUR, POP_URB, POP_FEM, POP_MAS and POP_TOT

--- src/datapreparation/step_03_data_cleaning.R
+++ src/datapreparation/step_03_data_cleaning.R
@@ -12,12 +12,32 @@ mytable_empty_cols <-  sapply(target@data, function(x) table(as.character(x) == 
 # for the missing columns based on the average values for the rest of
 # cities in Minas Gerais.
 
-# converting factor columns from text to numeric
+# converting columns from text to numeric
 target$POP_RUR <- as.numeric(as.character(target$POP_RUR))
 target$POP_URB <- as.numeric(as.character(target$POP_URB))
 target$POP_FEM <- as.numeric(as.character(target$POP_FEM))
 target$POP_MAS <- as.numeric(as.character(target$POP_MAS))
 
-# TO-DO: tratar as colunas com valor zero
+# getting rural population and female's mean ratio
 target.dataframe <- as(target, "data.frame")
-temp <- filter(target.dataframe, POP_RUR == 0)
+target.dataframe <- filter(target.dataframe, POP_RUR != 0)
+poprur.ratio <- mean(target.dataframe$POP_RUR) / mean(target.dataframe$POP_URB)
+pophm.ratio  <- mean(target.dataframe$POP_MAS) / mean(target.dataframe$POP_FEM)
+popfem.ratio <- mean(target.dataframe$POP_FEM) / mean(target.dataframe$POP_TOT)
+
+# adjusting rows with zero in POP_RUR, POP_URB, POP_FEM, POP_MAS and POP_TOT
+target$POP_RUR <- ifelse(target$POP_RUR == 0, 
+                         round(target$POP_94 * poprur.ratio, digits = 0),
+                         round(target$POP_RUR, digits = 0))
+target$POP_URB <- ifelse(target$POP_URB == 0, 
+                         round(target$POP_94 - target$POP_RUR, digits = 0),
+                         round(target$POP_URB, digits = 0))
+target$POP_FEM <- ifelse(target$POP_FEM == 0, 
+                         round(target$POP_94 * popfem.ratio, digits = 0),
+                         round(target$POP_FEM, digits = 0))
+target$POP_MAS <- ifelse(target$POP_MAS == 0, 
+                         round(target$POP_94 - target$POP_FEM, digits = 0),
+                         round(target$POP_MAS, digits = 0))
+target$POP_TOT <- ifelse(target$POP_TOT == 0, 
+                         round(target$POP_94, digits = 0),
+                         round(target$POP_TOT, digits = 0))
