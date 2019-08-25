1ffe852e11c1af6145680cb0fb1e34f0513b6e8e
ldaniel <contato@leandrodaniel.com>
2019-08-24 22:34:42 -0300

    Removing write shapefile code from here to step 06

--- src/datapreparation/step_03_data_cleaning.R
+++ src/datapreparation/step_03_data_cleaning.R
@@ -14,10 +14,4 @@ mytable_empty_cols <-  sapply(target@data, function(x) table(as.character(x) == 
 
 # TO-DO: tratar as colunas com valor zero
 
-dataProcessedDirectory <- "./data/processed/"
-shapefile_to_write <- paste(dataProcessedDirectory, "crime_mg_processed.shp", sep = "")
-writeOGR(target, 
-         dsn = shapefile_to_write, 
-         driver ="ESRI Shapefile", 
-         layer = "cities", 
-         overwrite_layer = TRUE)
+
